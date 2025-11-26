4ad2f2863e921a1f8de34aa98a189558213e499b
lucadeiras <luca.ap13@gmail.com>
2025-11-26 15:56:22 -0300

    Update splash_screen.py

--- src/utils/splash_screen.py
+++ src/utils/splash_screen.py
@@ -8,7 +8,7 @@ def show(uri='mongodb://localhost:27017', dbname='biblioteca'):
     counts = {c: db[c].count_documents({}) for c in ['livros','alunos','emprestimos'] if c in db.list_collection_names()}
     print('===================================================')
     print('Sistema: Biblioteca Escolar - C3 (Trabalho de BD NoSQL)')
-    print('Componentes do grupo: <Coloque os nomes aqui>')
+    print('Componentes do grupo: Lucas Pires de Alencar')
     print('Professor: Howard Roatti')
     print('Disciplina: Banco de Dados')
     print('Data:', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
