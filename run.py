476fb7902306ff8f6fbc5834a8239b6a3002ed07
lucadeiras <luca.ap13@gmail.com>
2025-11-26 15:52:37 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 import subprocess
 import sys
 
-print("🚀 Iniciando o sistema da Biblioteca Escolar...\n")
+print(" Iniciando o sistema da Biblioteca Escolar...\n")
 
-# 1️⃣ Executa a criação das tabelas e inserção de dados
-print("🧱 Etapa 1: Criando tabelas e registros iniciais...\n")
+# Executa a criação das tabelas e inserção de dados
+print(" Etapa 1: Criando tabelas e registros iniciais...\n")
 subprocess.run([sys.executable, "-m", "src.create_tables_and_records"])
 
-# 2️⃣ Executa o sistema principal
+# Executa o sistema principal
 print("\n🎬 Etapa 2: Iniciando o sistema principal...\n")
 subprocess.run([sys.executable, "-m", "src.principal"])
